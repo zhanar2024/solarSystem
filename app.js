2982e4f82407b543708db639e71392403453b730
zhanar2024 <160571337+zhanar2024@users.noreply.github.com>
2024-06-28 15:17:16 -0400

    uncommented some lines of code

--- app.js
+++ app.js
@@ -25,47 +25,47 @@ const solarSystem = [
 
 // 1. Print the array of Jupiter's moons to the console (no for loop, just print the entire array).
 
-// console.log(solarSystem[4].moons);
+console.log(solarSystem[4].moons);
 
-// // 2. Print the name of Neptune's moon "Nereid" to the console.
+// 2. Print the name of Neptune's moon "Nereid" to the console.
 
-// console.log(solarSystem[7].moons[1]);
+console.log(solarSystem[7].moons[1]);
 
-// // 3. Add a new moon called "Endor" to Venus' moons array.
+// 3. Add a new moon called "Endor" to Venus' moons array.
 
-// solarSystem[1].moons.push("Endor");
-// console.log(solarSystem[1].moons);
+solarSystem[1].moons.push("Endor");
+console.log(solarSystem[1].moons);
 
-// // 4. Add a Pluto object to the solarSystem array using .push. The object should contain Pluto's name, ringSystem boolean, and moons array (which includes "Charon").
+// 4. Add a Pluto object to the solarSystem array using .push. The object should contain Pluto's name, ringSystem boolean, and moons array (which includes "Charon").
 
-// solarSystem.push({ name: "Pluto", ringSystem: true, moons: ["Charon"] });
-// console.log(solarSystem);
+solarSystem.push({ name: "Pluto", ringSystem: true, moons: ["Charon"] });
+console.log(solarSystem);
 
-// // 5. Add a new key value pair to the the Earth object: the key should be 'diameter', and the value should be Earth's diameter in miles represented as a string.
+// 5. Add a new key value pair to the the Earth object: the key should be 'diameter', and the value should be Earth's diameter in miles represented as a string.
 
-// solarSystem[2].diameter = "7917.5 miles";
-// console.log(solarSystem[2]);
-// // 6. Change Mercury's ringSystem boolean to true.
+solarSystem[2].diameter = "7917.5 miles";
+console.log(solarSystem[2]);
+// 6. Change Mercury's ringSystem boolean to true.
 
-// solarSystem[0].ringSystem = true;
-// console.log(solarSystem[0]);
+solarSystem[0].ringSystem = true;
+console.log(solarSystem[0]);
 
-// // 7. Change Uranus' moon "Umbriel" to "Oberon"
+// 7. Change Uranus' moon "Umbriel" to "Oberon"
 
-// const uranusMoons = solarSystem[6].moons;
-// const index = uranusMoons.indexOf("Umbriel");
-// if (index !== -1) {
-//   uranusMoons[index] = "Oberon";
-// }
-// console.log(solarSystem[6].moons);
+const uranusMoons = solarSystem[6].moons;
+const index = uranusMoons.indexOf("Umbriel");
+if (index !== -1) {
+  uranusMoons[index] = "Oberon";
+}
+console.log(solarSystem[6].moons);
 
 // 8. Iterate through the solarSystem array and print only the objects that have a ringSystem (where ringSystem: true), and ignore the others. You want to make a loop that goes over each item in the array, and include an if statement.
 
-// for (const planet of solarSystem) {
-//   if (planet.ringSystem) {
-//     console.log(planet);
-//   }
-// }
+for (const planet of solarSystem) {
+  if (planet.ringSystem) {
+    console.log(planet);
+  }
+}
 // The Solar System, Part 2
 const system = {
   mercury: {
